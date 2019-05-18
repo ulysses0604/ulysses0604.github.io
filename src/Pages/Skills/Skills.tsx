e8e92091fae2e524bfaa0485ef20dd5927f96a6b
Yuh Nish <ulysses0604@gmail.com>
2019-05-18 18:57:32 +0900

    move Skills contents to json

--- src/Pages/Skills/Skills.tsx
+++ src/Pages/Skills/Skills.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Toast, ToastBody, ToastHeader, Col, Container, Row } from 'reactstrap';
 
 import styles from './Skills.module.scss';
+import contents from './SkillsContents.json';
 
 class Skills extends React.Component {
   constructor(props: {}) {
@@ -14,46 +15,18 @@ class Skills extends React.Component {
         <h1 className={styles.title}>Skills</h1>
         <Container fluid={true} className={styles.container}>
           <Row>
-            <Col xs="12" lg="4">
-              <Toast className={styles.toast}>
-                <ToastHeader icon="info">
-                  Hoge1
+            { contents.map(item => (
+              <Col xs={12} lg={4}>
+                <Toast className={styles.toast}>
+                  <ToastHeader icon="info">
+                    {item.title}
                 </ToastHeader>
-                <ToastBody>
-                  HogeHoge
+                  <ToastBody>
+                    {item.text}
                 </ToastBody>
-              </Toast>
-            </Col>
-            <Col xs="12" lg="4">
-              <Toast className={styles.toast}>
-                <ToastHeader icon="info">
-                  Hoge2
-                </ToastHeader>
-                <ToastBody>
-                  HogeHoge
-                </ToastBody>
-              </Toast>
-            </Col>
-            <Col xs="12" lg="4">
-              <Toast className={styles.toast}>
-                <ToastHeader icon="info">
-                  Hoge3
-                </ToastHeader>
-                <ToastBody>
-                  HogeHoge
-                </ToastBody>
-              </Toast>
-            </Col>
-            <Col xs="12" lg="4">
-              <Toast className={styles.toast}>
-                <ToastHeader icon="info">
-                  Hoge4
-                </ToastHeader>
-                <ToastBody>
-                  HogeHoge
-                </ToastBody>
-              </Toast>
-            </Col>
+                </Toast>
+              </Col>
+            )) }
           </Row>
         </Container>
       </div>
