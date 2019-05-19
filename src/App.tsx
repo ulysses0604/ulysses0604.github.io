b14b3af30639c33044add898052a19b3b594de9d
Yuh Nish <ulysses0604@gmail.com>
2019-05-19 17:38:51 +0900

    apply styled-components to Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styles from './App.module.scss';
+import styled, { createGlobalStyle } from "styled-components";
 
 import Backdrop from './Components/Backdrop/Backdrop';
 import Navbar from './Components/Navbar/Navbar';
@@ -38,11 +38,12 @@ class App extends React.Component<{}, ISideDrawerState> {
 
     return (
       <Router>
-        <div className={styles.app}>
+        <div className="App">
+          <GlobalStyle />
           <Navbar drawToggleClickHandler={this.drawToggleClickHandler} />
           <SideDrawer {...sideDrawerOption} />
           {backDrop}
-          <div className={styles.main}>
+          <MainWrapper>
             <Switch>
               <Route path="/about" component={About} />
               <Route path="/works" component={Works} />
@@ -50,7 +51,7 @@ class App extends React.Component<{}, ISideDrawerState> {
               <Route path="/" component={Home} />
               <Route component={Home} />
             </Switch>
-          </div>
+          </MainWrapper>
         </div>
       </Router>
     );
@@ -70,3 +71,25 @@ class App extends React.Component<{}, ISideDrawerState> {
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    // background-image: url('./images/EchoCat.png');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: right bottom;
+    background-size: 38% 38%;
+  }
+`;
+
+const MainWrapper = styled.main`
+    height: 100%;
+    text-align: center;
+`;
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
-import { IconContext } from 'react-icons';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
 import { MdMenu } from "react-icons/md";
 
-import styles from  './Navbar.module.scss';
+import styled from "styled-components";
 
 interface IProps {
   drawToggleClickHandler(): void,
@@ -12,23 +12,20 @@ interface IProps {
 class Navbar extends React.Component<IProps, {}> {
   constructor(props: IProps) {
     super(props);
-    this.clickHandler = this.clickHandler.bind(this);
   };
 
   public render() {
     return (
-      <header className={styles.navbar} style={{ padding: 0 }}>
-        <nav className={styles.navigation}>
-          <div className={styles.toggleButton} onClick={this.clickHandler}>
+      <NavigationBar>
+        <Navigation>
+          <MenuIconWrapper onClick={this.clickHandler}>
             <IconContext.Provider value={{ color: "white", size: "1.5em" }}>
               <MdMenu />
             </IconContext.Provider>
-          </div>
-          <div>
-            <Link to="/" className={styles.title}>Portfolio</Link>
-          </div>
-          <div className={styles.spacer} style={{ flex: 1 }} />
-          <div className={styles.navigationItems}>
+          </MenuIconWrapper>
+          <TitleWrapper to="/">Yu Nishimura Portfolio</TitleWrapper>
+          <GrowWrapper />
+          <NavigationItemsWrapper>
             <ul>
               <Link to="/about">
                 <li>about</li>
@@ -40,15 +37,72 @@ class Navbar extends React.Component<IProps, {}> {
                 <li>skills</li>
               </Link>
             </ul>
-          </div>
-        </nav>
-      </header>
+          </NavigationItemsWrapper>
+        </Navigation>
+      </NavigationBar>
     );
   }
 
-  private clickHandler() {
+  private clickHandler = () => {
     this.props.drawToggleClickHandler();
-  }
+  };
 }
 
 export default Navbar;
+
+const NavigationBar = styled.header`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 56px;
+  background: #df4848;
+`;
+
+const Navigation = styled.nav`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  padding-left: 1rem;
+`;
+
+const MenuIconWrapper = styled.div`
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+const TitleWrapper = styled(Link)`
+  color: white;
+  font-size: 1.5rem;
+  padding: 0 1rem;
+  text-decoration: none;
+  @media (min-width: 769px) {
+    padding: 0 0rem;
+  }
+`;
+
+const GrowWrapper = styled.div`
+  flex: 1;
+`;
+
+const NavigationItemsWrapper = styled.div`
+  height: 100%;
+    & ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+  }
+    & li {
+      padding: 16px 1rem;
+  }
+    & a {
+      color: white;
+      text-decoration: none;
+  }
+    @media (max-width: 768px) {
+      display: none;
+  }
+`;
